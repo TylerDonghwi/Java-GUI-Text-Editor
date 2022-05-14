2e20eeb6d9192e410d03468b861275293d04e9ae
TylerDonghwi <tylerdonghwikim@email.com>
2022-05-14 21:13:05 +1200

    button to change text color and background color

--- src/TextEditorFrame.java
+++ src/TextEditorFrame.java
@@ -1,9 +1,12 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -19,9 +22,32 @@ public class TextEditorFrame extends JFrame implements ActionListener {
 	JScrollPane scrollPane;
 	JSpinner fontSizeSpinner;
 	JLabel fontLabel;
+	JButton textColourButton;
+	Color textColour;
+	JButton backgroundColourButton;
+	Color backgroundColour;
+	JColorChooser colourChooser;
 
 	TextEditorFrame() {
-		// initialise JFrame Properties
+
+		setJFrame(); // initialise JFrame Properties
+		setTextBox(); // Setting up the text area
+		setScrollPane(); // Scroll Pane for scroll bars
+		setFontSizeSpinner(); // font size spinner
+		setTextColourButton(); // text colour changer
+		setBackgroundColourButton(); // background colour changer
+
+		// Adding all the components together
+		this.add(fontLabel);
+		this.add(fontSizeSpinner);
+		this.add(textColourButton);
+		this.add(backgroundColourButton);
+		this.add(scrollPane);
+		this.setVisible(true);
+	}
+
+	public void setJFrame() {
+		// initialise JFrame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Text Editor");
 		this.setSize(600, 600);
@@ -29,22 +55,27 @@ public class TextEditorFrame extends JFrame implements ActionListener {
 		this.setLocationRelativeTo(null); // it will be created at the middle of the screen
 		this.setResizable(true);
 
-		// Setting up the text area
+	}
+
+	public void setTextBox() {
+		// initialise text area
 		textBox = new JTextArea();
 		textBox.setLineWrap(true);
 		textBox.setWrapStyleWord(true);
 		textBox.setFont(new Font("Consolas", Font.PLAIN, 11)); // default font is Consolas and size 11
+	}
 
-		// Scroll Pane for scroll bars
+	public void setScrollPane() {
 		scrollPane = new JScrollPane(textBox);
 		scrollPane.setPreferredSize(new Dimension(500, 500));
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-
 		// label for font spinner
 		fontLabel = new JLabel();
 		fontLabel.setText("Font:");
+	}
 
-		// font size spinner
+	public void setFontSizeSpinner() {
+		// set font size spinner
 		fontSizeSpinner = new JSpinner();
 		fontSizeSpinner.setPreferredSize(new Dimension(50, 25));
 		fontSizeSpinner.setValue(11);
@@ -56,17 +87,31 @@ public class TextEditorFrame extends JFrame implements ActionListener {
 			}
 
 		});
+	}
 
-		// Adding all the components together
-		this.add(fontLabel);
-		this.add(fontSizeSpinner);
-		this.add(scrollPane);
-		this.setVisible(true);
+	public void setTextColourButton() {
+		// for changing the colour of the text
+		textColourButton = new JButton("Text Color");
+		textColourButton.addActionListener(this);
+	}
+
+	public void setBackgroundColourButton() {
+		// for changing the colour of the text
+		backgroundColourButton = new JButton("Background Color");
+		backgroundColourButton.addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+		if (e.getSource() == textColourButton) {
+			colourChooser = new JColorChooser();
+			textColour = colourChooser.showDialog(null, "Text Colour", textColour);
+			textBox.setForeground(textColour);
+		}
+		if (e.getSource() == backgroundColourButton) {
+			colourChooser = new JColorChooser();
+			backgroundColour = colourChooser.showDialog(null, "Background Colour", backgroundColour);
+			textBox.setBackground(backgroundColour);
+		}
 	}
 }
