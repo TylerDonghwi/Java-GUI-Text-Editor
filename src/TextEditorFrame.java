e081737da95ac198e54face0672b8662e6e6be9c
TylerDonghwi <tylerdonghwikim@email.com>
2022-05-14 19:59:41 +1200

    fixed textfield to textArea for and added scrollpane

--- src/TextEditorFrame.java
+++ src/TextEditorFrame.java
@@ -6,11 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class TextEditorFrame extends JFrame implements ActionListener {
 
-	JTextField textField;
+	JTextArea textBox;
+	JScrollPane scrollPane;
 
 	TextEditorFrame() {
 		// initialise JFrame Properties
@@ -18,16 +20,22 @@ public class TextEditorFrame extends JFrame implements ActionListener {
 		this.setLayout(new FlowLayout());
 		this.setTitle("Text Editor");
 		this.setSize(500, 500);
+		this.setLocationRelativeTo(null); // it will be created at the middle of the screen
 		this.setResizable(true);
 
-		textField = new JTextField();
-		textField.setPreferredSize(new Dimension(500, 500));
-		textField.setFont(new Font("Consolas", Font.PLAIN, 11)); // default font is Consolas and size 11
-		textField.setForeground(new Color(0xFFFFFF)); // default color is black
-		textField.setBackground(Color.white);
-		textField.setCaretColor(Color.black);
+		textBox = new JTextArea();
+		textBox.setPreferredSize(new Dimension(500, 500));
+		textBox.setLineWrap(true);
+		textBox.setWrapStyleWord(true);
+		textBox.setFont(new Font("Consolas", Font.PLAIN, 11)); // default font is Consolas and size 11
+		textBox.setForeground(new Color(0x000000)); // default color is black
+		textBox.setBackground(Color.white);
+		textBox.setCaretColor(Color.black);
 
-		this.add(textField);
+		scrollPane = new JScrollPane(textBox);
+
+		this.add(scrollPane);
+		this.add(textBox);
 		this.pack();
 		this.setVisible(true);
 	}
