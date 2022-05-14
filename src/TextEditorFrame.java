faebe0a32e7f13d12b175c5393ed4edfd8f3ed63
TylerDonghwi <tylerdonghwikim@email.com>
2022-05-14 20:20:06 +1200

    font sizes can chnage

--- src/TextEditorFrame.java
+++ src/TextEditorFrame.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -6,37 +5,62 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class TextEditorFrame extends JFrame implements ActionListener {
 
 	JTextArea textBox;
 	JScrollPane scrollPane;
+	JSpinner fontSizeSpinner;
+	JLabel fontLabel;
 
 	TextEditorFrame() {
 		// initialise JFrame Properties
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout(new FlowLayout());
 		this.setTitle("Text Editor");
-		this.setSize(500, 500);
+		this.setSize(600, 600);
+		this.setLayout(new FlowLayout());
 		this.setLocationRelativeTo(null); // it will be created at the middle of the screen
 		this.setResizable(true);
 
+		// Setting up the text area
 		textBox = new JTextArea();
-		textBox.setPreferredSize(new Dimension(500, 500));
 		textBox.setLineWrap(true);
 		textBox.setWrapStyleWord(true);
 		textBox.setFont(new Font("Consolas", Font.PLAIN, 11)); // default font is Consolas and size 11
-		textBox.setForeground(new Color(0x000000)); // default color is black
-		textBox.setBackground(Color.white);
-		textBox.setCaretColor(Color.black);
 
+		// Scroll Pane for scroll bars
 		scrollPane = new JScrollPane(textBox);
+		scrollPane.setPreferredSize(new Dimension(500, 500));
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 
+		// label for font spinner
+		fontLabel = new JLabel();
+		fontLabel.setText("Font:");
+
+		// font size spinner
+		fontSizeSpinner = new JSpinner();
+		fontSizeSpinner.setPreferredSize(new Dimension(50, 25));
+		fontSizeSpinner.setValue(11);
+		fontSizeSpinner.addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				textBox.setFont(new Font(textBox.getFont().getFamily(), Font.PLAIN, (int) fontSizeSpinner.getValue()));
+			}
+
+		});
+
+		// Adding all the components together
+		this.add(fontLabel);
+		this.add(fontSizeSpinner);
 		this.add(scrollPane);
-		this.add(textBox);
-		this.pack();
 		this.setVisible(true);
 	}
 
